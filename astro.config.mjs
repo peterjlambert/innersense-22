7ce1ab0e022e1be3596e5542838c47e28bb9fc25
Pete Lambert <pete@petelambert.com>
2022-11-14 10:40:57 +0000

    Starts to implement Storyblok

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,34 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import svelte from "@astrojs/svelte";
+import storyblok from "@storyblok/astro";
+import { loadEnv } from 'vite';
+
+const env = loadEnv(import.meta.env.MODE, process.cwd(), 'STORYBLOK');
 
 export default defineConfig({
 	integrations: [
 		react(),
 		svelte(),
-	]
+		storyblok({
+			accessToken:
+				import.meta.env.MODE === 'development'
+				? env.STORYBLOK_PREVIEW_TOKEN
+				: env.STORYBLOK_PUBLIC_TOKEN,
+			components: {
+				// Add your components here
+				Button: 'storyblok/ButtonLink',
+				Card: 'storyblok/Card',
+				ContentBlock: 'storyblok/ContentBlock',
+				Hero: 'storyblok/Hero',
+				RelatedContent: 'storyblok/RelationGrid',
+				Treatment: 'storyblok/Treatment',
+				Page: 'storyblok/Page'
+			},
+			apiOptions: {
+				// Choose your Storyblok space region 
+				region: 'eu',
+			},
+		}),
+	],
 })
